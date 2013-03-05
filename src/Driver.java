2e1e638087d89c991358051bb46aa1a918ebc39f
Hasnain Ali Pirzada <alihpsse@gmail.com>
2013-03-05 14:39:30 +0500

    starting point

---
+++ src/Driver.java
@@ -0,0 +1,28 @@
+import java.math.*;
+
+
+
+
+/**
+ *
+ * @author Ali
+ */
+public class Driver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       
+        try{
+        String a = Hash.getHash("123456");
+        System.out.println(a);
+        //BigInteger bi = new BigInteger(a);
+        }catch(Exception e){}
+         
+        
+        
+    }
+}
